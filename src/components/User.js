6207b8a2800f89b96fb7daea6bb1233aa5b1b102
Marina Marques <9.marques.marina@gmail.com>
2019-04-02 17:18:07 +0100

    adds user information to list

--- src/components/User.js
+++ src/components/User.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 
 class User extends Component {
   render() {
-    const { image, name, username, email } = this.props.details;
+    const { picture, name, login, email } = this.props.details;
+    const full_name = name.first + ' ' + name.last;
     return (
-      <div className='user-info'>
-        <p className='first-name'>{name.first}</p>
+      <div className='user-info-card'>
+        <img src={picture.thumbnail} alt={full_name} />
+        <p className='name'>{full_name}</p>
+        <p className='username'>{login.username}</p>
+        <p className='email'>{email}</p>
       </div>
     );
   }
