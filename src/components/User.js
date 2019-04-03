cf635b76835243d5f6fec84bc5403b08222cbaa1
Marina Marques <9.marques.marina@gmail.com>
2019-04-03 14:18:09 +0100

    changes font and adjusts layout, adds '+' to expand user detail

--- src/components/User.js
+++ src/components/User.js
@@ -7,9 +7,14 @@ class User extends Component {
     return (
       <div className='user-info-card'>
         <img src={picture.thumbnail} alt={full_name} />
-        <p className='name'>{full_name}</p>
-        <p className='username'>{login.username}</p>
-        <p className='email'>{email}</p>
+        <div className='user-info-details'>
+          <p className='name'>{full_name}</p>
+          <p className='username'>{login.username}</p>
+          <p className='email'>{email}</p>
+          <div className='user-info-actions'>
+            <span>+</span>
+          </div>
+        </div>
       </div>
     );
   }
