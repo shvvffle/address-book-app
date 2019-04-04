4159587404fe3a901ab7b95c219243e9b56e8b40
Marina Marques <9.marques.marina@gmail.com>
2019-04-04 11:57:23 +0100

    adds click event to hide user detail

--- src/components/UserDetailModal.js
+++ src/components/UserDetailModal.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
 
 class UserDetailModal extends Component {
+  hideUserDetails = event => {
+    const user_email = event.currentTarget.getAttribute('data-value');
+    const user_detail = document.getElementById('user_' + user_email);
+    user_detail.classList.add('hidden');
+  };
   render() {
     const { picture, name, login, email } = this.props.details;
     const full_name = name.first + ' ' + name.last;
     return (
       <div className='user-info-modal hidden' id={'user_' + email}>
-        <img src={picture.thumbnail} alt={full_name} />
-        <div className='user-info-details'>
-          <p className='name'>{full_name}</p>
-          <p className='username'>{login.username}</p>
-          <p className='email'>{email}</p>
+        <div className='user-info-modal-main'>
+          <img src={picture.thumbnail} alt={full_name} />
+          <div className='user-info-details'>
+            <p className='name'>{full_name}</p>
+            <p className='username'>{login.username}</p>
+            <p className='email'>{email}</p>
+          </div>
+          <div
+            className='user-info-actions'
+            onClick={this.hideUserDetails}
+            data-value={email}
+          >
+            <span>x</span>
+          </div>
         </div>
       </div>
     );
