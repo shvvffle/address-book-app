259a8e0f0c56a1795667fd35c93c4e27510a6595
Marina Marques <9.marques.marina@gmail.com>
2019-04-05 17:06:55 +0100

    shows users in batch of 50, loads more on scroll, shows loading msg

--- src/components/App.js
+++ src/components/App.js
@@ -6,23 +6,57 @@ import Search from './Search';
 
 class App extends React.Component {
   state = {
-    users: {},
-    isLoading: true
+    users: [],
+    isLoading: true,
+    requestSent: true
   };
+
   componentDidMount() {
-    const max_results = 1000;
+    this.loadUsers();
+  }
+
+  loadUsers() {
     const min_results = 50;
     axios
       .get('https://randomuser.me/api/?results=' + min_results + '')
       .then(response => {
         const users = response.data.results;
+        const new_users = this.state.users.concat(users);
         this.setState({
-          users,
-          isLoading: false
+          users: new_users,
+          isLoading: false,
+          requestSent: false
         });
-        console.log(this.state);
       });
   }
+
+  querySearchResult() {
+    if (this.state.requestSent) {
+      return;
+    }
+
+    this.setState({ requestSent: true });
+
+    // enumerate a slow query
+    setTimeout(this.loadUsers.bind(this), 2000);
+  }
+
+  handleOnScroll() {
+    const wrapper = document.querySelector('.users-wrapper');
+    const scrollTop = wrapper && wrapper.scrollTop;
+    const scrollHeight = wrapper && wrapper.scrollHeight;
+    const clientHeight = window.innerHeight;
+    const scrolledToBottom =
+      Math.ceil(scrollTop + clientHeight) >= scrollHeight;
+    if (scrolledToBottom) {
+      if (this.state.users.length === 1000) {
+        // show end of catalogue
+        console.log('oi');
+      }
+      this.querySearchResult();
+    }
+  }
+
   render() {
     if (this.state.isLoading) {
       return <Loading />;
@@ -30,12 +64,24 @@ class App extends React.Component {
       return (
         <div className='wrapper'>
           <Search users={this.state.users} />
-          <div className='users-wrapper'>
+          <div
+            className='users-wrapper'
+            onScroll={this.handleOnScroll.bind(this)}
+          >
             <ul className='users'>
               {Object.keys(this.state.users).map(key => (
                 <User key={key} index={key} details={this.state.users[key]} />
               ))}
             </ul>
+          </div>
+          <div
+            className={
+              this.state.requestSent
+                ? 'loading-users show'
+                : 'loading-users hidden'
+            }
+          >
+            loading
           </div>
         </div>
       );
