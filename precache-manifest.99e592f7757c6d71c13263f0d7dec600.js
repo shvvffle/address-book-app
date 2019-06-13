75b21d6e1405e35b8ac2b5ed847da63067d6855f
Marina Marques <9.marques.marina@gmail.com>
2019-06-13 16:25:19 +0100

    Updates

---
+++ precache-manifest.99e592f7757c6d71c13263f0d7dec600.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d704a19c96c9219f3a3d",
+    "url": "/address-book-app/static/css/main.439b15ae.chunk.css"
+  },
+  {
+    "revision": "d704a19c96c9219f3a3d",
+    "url": "/address-book-app/static/js/main.b99088e8.chunk.js"
+  },
+  {
+    "revision": "6fbabff7d32a6139929f",
+    "url": "/address-book-app/static/js/runtime~main.cbabd62d.js"
+  },
+  {
+    "revision": "b82ba4a1bdc00601b664",
+    "url": "/address-book-app/static/js/2.333ef159.chunk.js"
+  },
+  {
+    "revision": "f67d6457fc7faecb6cc0b7ddc02289f1",
+    "url": "/address-book-app/index.html"
+  }
+];
